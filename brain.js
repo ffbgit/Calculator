13896a021f5a29a6e8631e0cc7c4123801e2f272
ffbgit <f.f.bodie@gmail.com>
2017-11-11 18:56:42 +0100

    brain file

---
+++ brain.js
@@ -0,0 +1,17 @@
+var Calculator = {
+  multiply: function(number1, number2) {
+    return number1 * number2;
+  },
+
+  divide: function(number1, number2) {
+    return number1/number2;
+  },
+
+  countDown: function(number1, number2) {
+    return number1-number2;
+  },
+
+  addUp: function(number1, number2) {
+    return (number1+number2);
+  }
+}
